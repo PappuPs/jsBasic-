c3eb62075c0bdf3f828f45a9a75f2b7e57d23fff
PappuPs <pappuislam67@gmail.com>
2024-05-04 19:09:28 +0600

    some ans add

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
+// 2
+// ans:
+
+// var a = 70;
+// let Fullname = "Pappu Islam";
+// const s = 7.895;
+
+
+// 3
+// ans:
+
+// "var" has a function or global scope and allows redeclaration and reassignment, 
+// "let" has block scope and allows reassignment but not redeclaration, 
+// and "const" has block scope and does not allow reassignment or redeclaration after initialization.
+
+
 // 4
+// ans:
+
 // console.log("Pappu");
 
 // 5
+// ans
+
 // alert("Pappu");
 
 
 
 
 // 6
+// ans
+
 // var firstNumber=12;
 // var secondNumber=10;
 
 // var sum = firstNumber + secondNumber;
 
 // console.log(sum);
+
 
 
 
