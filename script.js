b161504c3ebd1ee926d57a4c9a231b8ec8126992
PappuPs <pappuislam67@gmail.com>
2024-05-04 14:14:24 +0600

    Js Basic qus & ans

---
+++ script.js
@@ -0,0 +1,19 @@
+// 4
+// console.log("Pappu");
+
+// 5
+// alert("Pappu");
+
+
+
+
+// 6
+// var firstNumber=12;
+// var secondNumber=10;
+
+// var sum = firstNumber + secondNumber;
+
+// console.log(sum);
+
+
+
